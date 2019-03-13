bde7e2292b538b20a9875232b4aaab651147f33d
erhedev <erik.hede.dev@gmail.com>
2019-03-13 11:12:04 +0100

    Initial Commit

---
+++ Good Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good Weather
+//
+//  Created by Erik Hede on 13/03/2019.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
