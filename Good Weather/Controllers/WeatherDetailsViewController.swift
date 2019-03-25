662c4338bdc57fa3be912e22e14257b048f8e257
erik.hede.dev@gmail.com <erik.hede.dev@gmail.com>
2019-03-25 23:04:21 +0100

    vm to view bindings

--- Good Weather/Controllers/WeatherDetailsViewController.swift
+++ Good Weather/Controllers/WeatherDetailsViewController.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+class WeatherDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var maxTemp: UILabel!
+    @IBOutlet weak var minTemp: UILabel!
+    
+    var weatherViewModel: WeatherViewModel?
+    
+    
+    override func viewDidLoad() {
+        setupVWBingings()
+    
+    }
+    
+    func setupVWBingings() {
+        if let weatherVM = self.weatherViewModel {
+            weatherVM.name.bind { self.cityNameLabel.text = $0 }
+            weatherVM.currentTemperature.temperature.bind { self.currentTemp.text = $0.formatAsDegree }
+            
+        }
+    }
+    
+    
+    
+    
+    
+}
